2c7dd5a2fe37f2ebe829e995e2afca1ac30638bd
autroshot <autroshot@gamil.com>
2022-07-10 01:41:51 +0900

    리펙터링: Dice 컴포넌트의 지역변수 인라인 처리

--- src/components/Dice.tsx
+++ src/components/Dice.tsx
@@ -1,12 +1,12 @@
 import { DiceProps } from '../types/props';
 
 export default function Dice(props: DiceProps) {
-  const src = `images/dice-${props.value}.png`;
-  const alt = `주사위 ${props.value}`;
-
   return (
     <button className="dice">
-      <img src={src} alt={alt} />
+      <img
+        src={`images/dice-${props.value}.png`}
+        alt={`주사위 ${props.value}`}
+      />
     </button>
   );
 }
