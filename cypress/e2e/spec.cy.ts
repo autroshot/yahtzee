fb948fcabfec5d63d137e98cf797afc68671a96e
autroshot <autroshot@gamil.com>
2022-07-11 20:53:15 +0900

    테스트: 주사위 굴리기 행동 1회 작성

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -8,4 +8,15 @@ describe('게임', () => {
     cy.get('[data-cy="instruction"]').should('contain', '주사위를 굴리세요.');
     cy.get('[data-cy="rolled-dices"]').should('not.be.visible');
   });
+
+  it('주사위 굴리기 행동', () => {
+    cy.contains('주사위 굴리기').click();
+
+    cy.get('[data-cy="roll-count"]').should('contain', '1');
+    cy.get('[data-cy="instruction"]').should(
+      'contain',
+      '주사위를 다시 굴리거나 점수를 선택하세요.'
+    );
+    cy.get('[data-cy="rolled-dices"]').children().should('have.length', '6');
+  });
 });
