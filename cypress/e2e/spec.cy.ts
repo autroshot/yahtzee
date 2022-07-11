d96687311a171ed986452f7e66c59071dbe25481
autroshot <autroshot@gamil.com>
2022-07-11 20:56:01 +0900

    테스트: 주사위 굴리기 행동 여러 번 테스트 작성

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -18,5 +18,26 @@ describe('게임', () => {
       '주사위를 다시 굴리거나 점수를 선택하세요.'
     );
     cy.get('[data-cy="rolled-dices"]').children().should('have.length', '6');
+
+    cy.contains('주사위 굴리기').click();
+
+    cy.get('[data-cy="roll-count"]').should('contain', '2');
+    cy.get('[data-cy="instruction"]').should(
+      'contain',
+      '주사위를 다시 굴리거나 점수를 선택하세요.'
+    );
+    cy.get('[data-cy="rolled-dices"]').children().should('have.length', '6');
+
+    cy.contains('주사위 굴리기').click();
+
+    cy.get('[data-cy="roll-count"]').should('contain', '3');
+    cy.get('[data-cy="instruction"]').should('contain', '점수를 선택하세요.');
+    cy.get('[data-cy="rolled-dices"]').children().should('have.length', '6');
+
+    cy.contains('주사위 굴리기').click();
+
+    cy.get('[data-cy="roll-count"]').should('contain', '3');
+    cy.get('[data-cy="instruction"]').should('contain', '점수를 선택하세요.');
+    cy.get('[data-cy="rolled-dices"]').children().should('have.length', '6');
   });
 });
