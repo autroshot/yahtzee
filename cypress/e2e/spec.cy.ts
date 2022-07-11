c5e293b87100f30778350dd9e2ab5a1bd23ff8b3
autroshot <autroshot@gamil.com>
2022-07-11 21:25:58 +0900

    테스트: 주사위 굴리기 정리

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,4 +1,4 @@
-describe('게임', () => {
+describe('초깃값', () => {
   it('접속', () => {
     cy.visit('/');
   });
@@ -8,8 +8,14 @@ describe('게임', () => {
     cy.get('[data-cy="instruction"]').should('contain', '주사위를 굴리세요.');
     cy.get('[data-cy="rolled-dices"]').should('not.be.visible');
   });
+});
 
-  it('주사위 굴리기 행동', () => {
+describe('주사위 굴리기', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('제한 횟수 이내 주사위 굴리기', () => {
     cy.contains('주사위 굴리기').click();
 
     cy.get('[data-cy="roll-count"]').should('contain', '1');
@@ -33,11 +39,21 @@ describe('게임', () => {
     cy.get('[data-cy="roll-count"]').should('contain', '3');
     cy.get('[data-cy="instruction"]').should('contain', '점수를 선택하세요.');
     cy.get('[data-cy="rolled-dices"]').children().should('have.length', '6');
+  });
+
+  it('제한 횟수 초과 주사위 굴리기', () => {
+    for (let i = 0; i < 4; i++) {
+      cy.contains('주사위 굴리기').click();
+    }
 
     cy.contains('주사위 굴리기').click();
 
     cy.get('[data-cy="roll-count"]').should('contain', '3');
     cy.get('[data-cy="instruction"]').should('contain', '점수를 선택하세요.');
     cy.get('[data-cy="rolled-dices"]').children().should('have.length', '6');
+  });
+
+  it('주사위 값 변화 확인', () => {
+    cy.contains('주사위 굴리기').click();
   });
 });
