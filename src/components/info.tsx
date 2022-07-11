63a35635ba78630c74ea0bb69d5145853cfc9039
autroshot <autroshot@gamil.com>
2022-07-11 12:10:42 +0900

    Info 컴포넌트에서 props로 받은 rollCount 사용

--- src/components/info.tsx
+++ src/components/info.tsx
@@ -8,7 +8,7 @@ export default function Info(props: InfoProps) {
         <Col>라운드: 1/12</Col>
       </Row>
       <Row>
-        <Col>굴린 횟수: 0/3</Col>
+        <Col>굴린 횟수: {props.rollCount}/3</Col>
       </Row>
       <Row>
         <Col>주사위를 굴리세요.</Col>
