99c18d1a3f85bfc03c5ee9d7b2f13c28cbfb04eb
autroshot <autroshot@gamil.com>
2022-07-13 18:19:23 +0900

    테스트: 모든 점수 span에 data-cy 속성 추가

--- src/components/ScoreCard.tsx
+++ src/components/ScoreCard.tsx
@@ -17,58 +17,84 @@ export default function ScoreCard(props: ScoreCardProps) {
             <tbody>
               <tr>
                 <td>에이스</td>
-                <td>{scores.ace}</td>
+                <td>
+                  <span data-cy="ace">{scores.ace}</span>
+                </td>
               </tr>
               <tr>
                 <td>듀얼</td>
-                <td>{scores.dual}</td>
+                <td>
+                  <span data-cy="dual">{scores.dual}</span>
+                </td>
               </tr>
               <tr>
                 <td>트리플</td>
-                <td>{scores.triple}</td>
+                <td>
+                  <span data-cy="triple">{scores.triple}</span>
+                </td>
               </tr>
               <tr>
                 <td>쿼드</td>
-                <td>{scores.quad}</td>
+                <td>
+                  <span data-cy="quad">{scores.quad}</span>
+                </td>
               </tr>
               <tr>
                 <td>펜타</td>
-                <td>{scores.penta}</td>
+                <td>
+                  <span data-cy="penta">{scores.penta}</span>
+                </td>
               </tr>
               <tr>
                 <td>헥사</td>
-                <td>{scores.hexa}</td>
+                <td>
+                  <span data-cy="hexa">{scores.hexa}</span>
+                </td>
               </tr>
               <tr className="total-score">
                 <td>상단 점수의 합이 63점 이상이라면</td>
                 <td>
-                  {calculateUpperTotal(scores)}
+                  <span data-cy="upper-total">
+                    {calculateUpperTotal(scores)}
+                  </span>
                   /63
                 </td>
               </tr>
               <tr className="total-score">
                 <td>상단 보너스 +35점</td>
-                <td>{calculateBonus(scores)}</td>
+                <td>
+                  <span data-cy="bonus">{calculateBonus(scores)}</span>
+                </td>
               </tr>
               <tr>
                 <td>초이스</td>
-                <td>{scores.choice}</td>
+                <td>
+                  <span data-cy="choice">{scores.choice}</span>
+                </td>
               </tr>
               <tr>
                 <td>포커</td>
-                <td>{scores.poker}</td>
+                <td>
+                  <span data-cy="poker">{scores.poker}</span>
+                </td>
               </tr>
               <tr>
                 <td>풀 하우스</td>
-                <td>{scores.fullHouse}</td>
+                <td>
+                  <span data-cy="full-house">{scores.fullHouse}</span>
+                </td>
               </tr>
               <tr>
                 <td>스몰 스트레이트</td>
-                <td>{scores.smallStraight}</td>
+                <td>
+                  <span data-cy="small-straight">{scores.smallStraight}</span>
+                </td>
               </tr>
               <tr>
                 <td>라지 스트레이트</td>
-                <td>{scores.largeStraight}</td>
+                <td>
+                  <span data-cy="large-straight">{scores.largeStraight}</span>
+                </td>
               </tr>
               <tr>
                 <td>요트</td>
@@ -76,7 +102,9 @@ export default function ScoreCard(props: ScoreCardProps) {
               </tr>
               <tr className="total-score">
                 <td>총점</td>
-                <td>{calculateTotal(scores)}</td>
+                <td>
+                  <span data-cy="total">{calculateTotal(scores)}</span>
+                </td>
               </tr>
             </tbody>
           </Table>
