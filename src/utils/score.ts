7fb49d732f5b37c8aaf30f2a6e595018cf39ddc5
autroshot <autroshot@gamil.com>
2022-07-17 15:56:06 +0900

    리팩터링: 미사용 score util 제거.

--- src/utils/score.ts
+++ src/utils/score.ts
@@ -16,34 +16,3 @@ export function createInitialScores(): Scores {
     yacht: null,
   };
 }
-
-export function calculateUpperTotal(scores: Scores) {
-  return (
-    (scores.ace ?? 0) +
-    (scores.dual ?? 0) +
-    (scores.triple ?? 0) +
-    (scores.quad ?? 0) +
-    (scores.penta ?? 0) +
-    (scores.hexa ?? 0)
-  );
-}
-
-export function calculateBonus(scores: Scores) {
-  if (calculateUpperTotal(scores) >= 63) {
-    return 35;
-  } else {
-    return 0;
-  }
-}
-
-export function calculateTotal(scores: Scores) {
-  let result = 0;
-
-  result += Object.values(scores).reduce((sum, score) => {
-    return (sum as number) + (score ?? 0);
-  }, 0) as number;
-
-  result += calculateBonus(scores);
-
-  return result;
-}
